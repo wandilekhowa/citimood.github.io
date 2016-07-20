0bfa2c95e96115e1eb5f1436b5759ee8817c0c94
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-20 13:07:46 +0200

    adding project files

---
+++ script/angular_script.js
@@ -0,0 +1,246 @@
+
+var app = angular.module("citymoodApp", ["firebase","ngRoute"]) 
+
+app.run(["$rootScope", "$location", function($rootScope, $location) 
+{
+  $rootScope.$on("$routeChangeError", function(event, next, previous, error) 
+  {
+    if (error === "AUTH_REQUIRED") 
+	{
+      $location.path("/");
+    }
+  });
+}]);
+
+app.config( function($routeProvider) 
+{
+  $routeProvider.when('/', 
+  {
+   controller: 'SignInCtrl',
+    templateUrl: 'templates/sign_in.html',
+    resolve: 
+	{ 
+      "currentAuth":  function($firebaseAuth) 
+	  {
+        return $firebaseAuth().$waitForSignIn();
+      }
+    }
+  }) .when('/profile/:id/:name/:hometown', 
+  {
+    controller:'ProfileCtrl',
+    templateUrl:'templates/profile.html',
+    resolve: 
+	{
+     "currentAuth":function($firebaseAuth) 
+	  {
+        return $firebaseAuth().$waitForSignIn();
+      }
+    }
+  })/*.when('/signup', 
+  {
+    controller: 'SignupCtrl',
+    templateUrl: 'templates/signup.html',
+    resolve: 
+	{
+     "currentAuth":function($firebaseAuth) 
+	  {
+        return $firebaseAuth().$waitForSignIn();
+      }
+    }
+  }).when('/channel/:channelId', 
+  {
+    controller: 'ChannelCtrl',
+    templateUrl: 'templates/channel.html',
+    resolve: 
+	{
+     "currentAuth":function($firebaseAuth) 
+	  {
+        return $firebaseAuth().$requireSignIn();
+      }
+    }
+  }) */;
+});
+
+app.controller("AppController", function($scope, $firebaseArray, $firebaseAuth,$routeParams,$location,$window,$location) 
+{
+	/* $scope.authObj = $firebaseAuth();
+	$scope.sign_out=function()
+	{
+		localStorage.greeted=0;
+		$scope.authObj.$signOut()
+		$location.path("/");
+	} */
+});
+
+app.controller("ProfileCtrl", function($scope, $firebaseArray, $firebaseAuth,$routeParams,$location,$window,currentAuth) 
+{
+	console.log($routeParams.id);		
+		
+		/* $scope.authObj = $firebaseAuth();
+		$scope.login= function()
+		{	
+
+			$scope.authObj.$signInWithEmailAndPassword($scope.email, $scope.password)
+			.then(function(firebaseUser)
+			{
+				$scope.nagative_message=false;
+				console.log("Signed in as:", firebaseUser.uid);
+				$location.path("/channels");
+				
+			}).catch(function(error) 
+			{
+				$scope.nagative_message=true;
+				console.error("Authentication failed:", error);
+			});
+		}; */
+
+});
+
+app.controller("SignInCtrl", function($scope, $firebaseArray, $firebaseAuth,$routeParams,$location,$window,currentAuth) 
+{
+		
+
+
+});
+
+ /*  app.controller("SignupCtrl", function($scope, $firebaseArray, $firebaseAuth,$routeParams) 
+ {
+    $scope.authObj = $firebaseAuth();
+	$scope.signUp= function() 
+	  {
+		$scope.authObj.$createUserWithEmailAndPassword($scope.email, $scope.password)
+		  .then(function(firebaseUser) 
+		  {
+			var ref = firebase.database().ref().child("Users");
+			$scope.users = $firebaseArray(ref);
+			$scope.users.$add
+			({
+			  userName:$scope.name,
+			  userId:firebaseUser.uid,
+			  created_at:Date.now()
+			});
+		    window.location.href="#/channels";
+			console.log("User " + firebaseUser.uid + " created successfully!");
+		  }).catch(function(error) 
+		  {
+			$scope.nagative_message=true;
+			console.error("Error: ", error);
+		  });
+	   };
+ });
+
+
+
+	
+app.controller("HomeCtrl", function($firebaseObject,$document,$scope, $firebaseArray, $firebaseAuth,$routeParams,$location,$window,currentAuth,$location) 
+{
+	
+	 if(localStorage.greeted!=1)
+	 {
+		angular.element(document.querySelector( '#lightbox' )).addClass('isVisible');
+		var shadow = document.querySelectorAll(".lightbox")[0];
+		
+		shadow.onclick = function()
+		{
+			angular.element(document.querySelector( '#lightbox' )).removeClass('isVisible ');
+
+			
+			localStorage.greeted=1;
+			
+		}
+	  }
+	  
+	 /*+++++++++++++++++++++++++++++++++++++++++++++++++++*
+	   var users = firebase.database().ref().child("Users");
+	   var users_obj = $firebaseObject(users);
+	
+	   //get Logged in user ID
+	   $scope.authObj = $firebaseAuth();
+	   var firebaseUser = $scope.authObj.$getAuth();
+		
+	   users_obj.$loaded().then(function() {
+		   angular.forEach(users_obj, function(value, key) 
+		  {
+			if(firebaseUser.uid==value.userId)
+			{
+			 $scope.name=value.userName;
+			}
+          }); 
+       });
+	   /*+++++++++++++++++++++++++++++++++++++++++++++++++++*
+	
+	
+
+
+  var ref = firebase.database().ref().child("channels");
+	/////////////==New Channel==///////////////
+	$scope.channels = $firebaseArray(ref);
+	$scope.createChannel = function() 
+	{
+		$scope.channels.$add({
+		channel:$scope.channelName,
+		created_at:Date.now()
+		});
+	};
+
+  
+});
+
+ app.controller("ChannelCtrl", function($scope,$firebaseObject, $firebaseArray, $firebaseAuth,$routeParams,currentAuth,$location) 
+ {
+ 
+	   /*+++++++++++++++++++++++++++++++++++++++++++++++++++*
+	   var users = firebase.database().ref().child("Users");
+	   var users_obj = $firebaseObject(users);
+
+	   //get Logged in user ID
+	   $scope.authObj = $firebaseAuth();
+	   var firebaseUser = $scope.authObj.$getAuth();
+		
+	   users_obj.$loaded().then(function() {
+		   angular.forEach(users_obj, function(value, key) 
+		  {
+			if(firebaseUser.uid==value.userId)
+			{
+			 $scope.name=value.userName;
+			}
+          }); 
+       });
+	   /*+++++++++++++++++++++++++++++++++++++++++++++++++++*
+		
+ 
+	   var ref = firebase.database().ref().child("messages").child($routeParams.channelId);
+	   $scope.messages = $firebaseArray(ref);
+
+	  $scope.channel_id=$routeParams.channelId;
+	  /////////////==New Message==//////////////
+	  $scope.sendMessage = function() 
+	  {
+		$scope.messages.$add({
+		  sender:$scope.name,
+		  text: $scope.newMessage, 
+		  created_at:Date.now()
+		});
+		
+		$scope.newMessage="";
+	  }; 
+
+	  
+ });
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
